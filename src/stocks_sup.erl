4ba2bc8eb8f0336c9c18bac23a78d2031a1c0c76
Erik Axling <dude@CodeMachine.(none)>
2010-11-17 00:38:04 +0100

    *** empty log message ***

--- src/stocks_sup.erl
+++ src/stocks_sup.erl
@@ -45,7 +45,10 @@ init([]) ->
     AnalysisHandler = {analysis_handler,
 		       {analysis_handler,start_link,[]},
 		       permanent,2000,worker,[analysis_handler]},
-    {ok,{{one_for_all,0,1}, [ContentHandler, AnalysisHandler]}}.
+    DbMysql = {db_mysql,
+		       {db_mysql,start_link,[]},
+		       permanent,2000,worker,[db_mysql]},
+    {ok,{{one_for_one,2,2}, [ContentHandler, AnalysisHandler, DbMysql]}}.
 
 %%====================================================================
 %% Internal functions
